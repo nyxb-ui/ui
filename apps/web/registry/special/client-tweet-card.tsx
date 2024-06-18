c70988471649908f0383e3a26ac22e029be4f266
nyxb <dennis.ollhoff@joypix.gg>
2024-06-18 08:56:03 +0200

    ✨ feat(apps/web/registry/special): add multiple animated components for enhanced UI effects
    This commit introduces a variety of new React components designed to enhance the user interface with animations and special effects. These components include animated grids, lines, lists, text, buttons, and more, each tailored to provide dynamic, visually appealing user interactions. The implementation leverages technologies like Framer Motion for smooth animations and React's latest features for optimal performance and flexibility. This addition aims to improve user engagement and the overall aesthetic of the web applications.

---
+++ apps/web/registry/special/client-tweet-card.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import type { TweetProps } from 'react-tweet'
+import { useTweet } from 'react-tweet'
+import {
+   MagicTweet,
+   TweetNotFound,
+   TweetSkeleton,
+} from '~/registry/special/tweet-card'
+
+function ClientTweetCard({
+   id,
+   apiUrl,
+   fallback = <TweetSkeleton />,
+   components,
+   fetchOptions,
+   onError,
+   ...props
+}: TweetProps & { className?: string }) {
+   const { data, error, isLoading } = useTweet(id, apiUrl, fetchOptions)
+
+   if (isLoading)
+      return fallback
+   if (error || !data) {
+      const NotFound = components?.TweetNotFound || TweetNotFound
+      return <NotFound error={onError ? onError(error) : error} />
+   }
+
+   return <MagicTweet tweet={data} components={components} {...props} />
+}
+
+export default ClientTweetCard
