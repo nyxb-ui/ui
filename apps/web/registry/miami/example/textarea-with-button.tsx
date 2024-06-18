6d65f67e2751f84bf3311a22776636a7cbb1f72e
nyxb <dennis.ollhoff@joypix.gg>
2024-06-18 08:59:08 +0200

    ✨ feat(apps/web/registry/miami): add multiple UI component examples for enhanced development and testing

---
+++ apps/web/registry/miami/example/textarea-with-button.tsx
@@ -0,0 +1,11 @@
+import { Button } from '~/registry/miami/ui/button'
+import { Textarea } from '~/registry/miami/ui/textarea'
+
+export default function TextareaWithButton() {
+   return (
+      <div className="grid w-full gap-2">
+         <Textarea placeholder="Type your message here." />
+         <Button>Send message</Button>
+      </div>
+   )
+}
---
+++ apps/web/registry/miami/example/toast-simple.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { Button } from '~/registry/miami/ui/button'
+import { useToast } from '~/registry/miami/ui/use-toast'
+
+export default function ToastSimple() {
+   const { toast } = useToast()
+
+   return (
+      <Button
+         variant="outline"
+         onClick={() => {
+            toast({
+               description: 'Your message has been sent.',
+            })
+         }}
+      >
+         Show Toast
+      </Button>
+   )
+}
