a06d094029736eadc63257c208877776232f0145
nyxb <dennis.ollhoff@joypix.gg>
2024-06-18 08:49:23 +0200

    ✨ feat(cli): add new T3 app template with Next.js, TailwindCSS, and TypeScript configurations
    This commit introduces a new T3 app template within the CLI package, providing a pre-configured setup for Next.js, TailwindCSS, and TypeScript. This setup includes essential configurations and dependencies to kickstart a new project, ensuring best practices and a streamlined development process. The addition includes:
    
    - `.gitignore` to ensure proper ignore rules for Node.js and Next.js projects.
    - `next.config.mjs` for advanced Next.js configurations including i18n setup.
    - `package.json` with necessary scripts and dependencies for building, developing, and linting the application.
    - `env.mjs` for managing environment variables with validation using Zod.
    - Basic page setup in `_app.tsx` and `index.tsx` to start with a functional Next.js app.
    - Global styles with TailwindCSS integrated in `globals.css`.
    - Tailwind configuration in `tailwind.config.ts` to customize TailwindCSS.
    - TypeScript configuration in `tsconfig.json` to ensure type safety and optimal developer experience.
    - A test setup in `is-typescript-project.test.ts` to validate TypeScript configuration in projects.
    
    This comprehensive setup aims to provide a robust starting point for developers using the T3 stack, encapsulating best practices and essential tools for modern web development.

---
+++ packages/cli/test/utils/is-typescript-project.test.ts
@@ -0,0 +1,37 @@
+import path from 'node:path'
+import { describe, expect } from 'vitest'
+
+import { isTypeScriptProject } from '../../src/utils/get-project-info'
+
+describe('is TypeScript project', async () => {
+   it.each([
+      {
+         name: 'next-app',
+         result: true,
+      },
+      {
+         name: 'next-app-src',
+         result: true,
+      },
+      {
+         name: 'next-pages',
+         result: true,
+      },
+      {
+         name: 'next-pages-src',
+         result: true,
+      },
+      {
+         name: 't3-app',
+         result: true,
+      },
+      {
+         name: 'next-app-js',
+         result: false,
+      },
+   ])(`isTypeScriptProject($name) -> $result`, async ({ name, result }) => {
+      expect(
+         await isTypeScriptProject(path.resolve(__dirname, `../fixtures/${name}`)),
+      ).toBe(result)
+   })
+})
