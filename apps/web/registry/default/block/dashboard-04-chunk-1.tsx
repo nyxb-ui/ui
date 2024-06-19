5ab197abc301a27a24b7f49d48416d1f205bcec9
nyxb <dennis.ollhoff@joypix.gg>
2024-06-19 07:43:59 +0200

    🩹 fix: update server port variable to uppercase and add environment variable support for port configuration

--- apps/web/registry/default/block/dashboard-04-chunk-1.tsx
+++ apps/web/registry/default/block/dashboard-04-chunk-1.tsx
@@ -1,24 +1,22 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '~/registry/default/ui/card'
-import { Button } from '~/registry/default/ui/button'
-import { Input } from '~/registry/default/ui/input'
 
-export default function Component() {
-   return (
-      <Card x-chunk="dashboard-04-chunk-1">
-         <CardHeader>
-            <CardTitle>Store Name</CardTitle>
-            <CardDescription>
-               Used to identify your store in the marketplace.
-            </CardDescription>
-         </CardHeader>
-         <CardContent>
-            <form>
-               <Input placeholder="Store Name" />
-            </form>
-         </CardContent>
-         <CardFooter className="border-t px-6 py-4">
-            <Button>Save</Button>
-         </CardFooter>
-      </Card>
-   )
-}
+             import {Card,CardHeader,CardTitle,CardDescription,CardContent,CardFooter} from "~/registry/default/ui/card"
+import {Button} from "~/registry/default/ui/button"
+import {Input} from "~/registry/default/ui/input"
+             export default function Component() {
+               return (<Card x-chunk="dashboard-04-chunk-1">
+                     <CardHeader>
+                        <CardTitle>Store Name</CardTitle>
+                        <CardDescription>
+                           Used to identify your store in the marketplace.
+                        </CardDescription>
+                     </CardHeader>
+                     <CardContent>
+                        <form>
+                           <Input placeholder="Store Name" />
+                        </form>
+                     </CardContent>
+                     <CardFooter className="border-t px-6 py-4">
+                        <Button>Save</Button>
+                     </CardFooter>
+                  </Card>);
+             }
