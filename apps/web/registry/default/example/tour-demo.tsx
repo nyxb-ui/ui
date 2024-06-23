4a51ce2a3f6ff4d7aaedaa07db5db7f27fa6515c
nyxb <dennis.ollhoff@joypix.gg>
2024-06-23 17:09:33 +0200

    ✨ feat(tour-demo.tsx): add 'use client' directive for improved client-side execution handling in React components

--- apps/web/registry/default/example/tour-demo.tsx
+++ apps/web/registry/default/example/tour-demo.tsx
@@ -1,3 +1,4 @@
+'use client'
 import * as React from 'react'
 
 import { Button } from '~/registry/default/ui/button'
--- apps/web/registry/miami/example/tour-demo.tsx
+++ apps/web/registry/miami/example/tour-demo.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { createContext, useContext, useEffect, useMemo, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
 
