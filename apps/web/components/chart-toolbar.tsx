6c39c679b24feb2b420a005a3a67d026706e5a34
nyxb <dennis.ollhoff@joypix.gg>
2024-07-08 16:35:58 +0200

    ✨ feat(charts): add various chart components and pages for chart display
    - Added multiple chart components for different chart types.
    - Created pages and layouts for displaying charts.
    - Implemented chart theme switcher and chart code viewer.
    - Added documentation for chart components and usage.

---
+++ apps/web/components/chart-toolbar.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import {
+   AreaChart,
+   BarChartBig,
+   Hexagon,
+   LineChart,
+   PieChart,
+   Radar,
+} from 'lucide-react'
+import { ny } from '~/lib/utils'
+import { BlockCopyButton } from '~/components/block-copy-button'
+import { ChartCodeViewer } from '~/components/chart-code-viewer'
+import { Separator } from '~/registry/miami/ui/separator'
+import type { Block } from '~/registry/schema'
+
+import '~/styles/mdx.css'
+
+export function ChartToolbar({
+   chart,
+   className,
+   children,
+}: { chart: Block } & React.ComponentProps<'div'>) {
+   return (
+      <div className={ny('flex items-center gap-2', className)}>
+         <div className="text-muted-foreground flex items-center gap-1.5 pl-1 text-[13px] [&>svg]:size-[0.9rem]">
+            {chart.subcategory === 'Line' && <LineChart />}
+            {chart.subcategory === 'Bar' && <BarChartBig />}
+            {chart.subcategory === 'Pie' && <PieChart />}
+            {chart.subcategory === 'Area' && <AreaChart />}
+            {chart.subcategory === 'Radar' && <Hexagon />}
+            {chart.subcategory === 'Radial' && <Radar />}
+            {chart.subcategory}
+            {' '}
+            Chart
+         </div>
+         <div className="ml-auto flex items-center gap-2 [&>form]:flex">
+            <BlockCopyButton
+               // @ts-expect-error is fine
+               event="copy_chart_code"
+               name={chart.name}
+               code={chart.code}
+               className="[&_svg]-h-3 text-foreground hover:bg-muted dark:text-foreground size-6 rounded-[6px] bg-transparent shadow-none [&_svg]:w-3"
+            />
+            <Separator orientation="vertical" className="mx-0 hidden h-4 md:flex" />
+            <ChartCodeViewer chart={chart}>{children}</ChartCodeViewer>
+         </div>
+      </div>
+   )
+}
