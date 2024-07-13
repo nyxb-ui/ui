66b3a8de08e35a655d902df1147094eda0fa0640
nyxb <dennis.ollhoff@joypix.gg>
2024-07-13 16:24:59 +0200

    🩹 fix(route.tsx): add console logs for debugging and improve error handling

--- apps/web/app/api/repo/download/route.tsx
+++ apps/web/app/api/repo/download/route.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 import { z } from 'zod'
@@ -13,6 +14,9 @@ export async function POST(request: NextRequest) {
    const body = await request.json()
    try {
       const { owner, repo } = bodySchema.parse(body)
+      console.log('Owner:', owner)
+      console.log('Repo:', repo)
+
       const response = await octokit.request(
          'GET /repos/{owner}/{repo}/zipball',
          {
@@ -23,12 +27,25 @@ export async function POST(request: NextRequest) {
             },
          },
       )
+
+      if (response.status !== 200) {
+         console.error('GitHub API Error:', response.status, response.data)
+         return NextResponse.json(
+            { success: false, error: 'GitHub API request failed' },
+            { status: 500 },
+         )
+      }
+
+      console.log('GitHub API response:', response)
+
       const status = response.status
       const downloadUrl = response.url
+      console.log('Download URL:', downloadUrl)
+
       return NextResponse.json({ success: true, status, downloadUrl })
    }
    catch (error: any) {
-      console.error('GitHub API Error:', error)
+      console.error('Error in /api/repo/download:', error)
       return NextResponse.json(
          { success: false, error: error.message },
          { status: 500 },
