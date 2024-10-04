cabf5d551b76ec40b1faeaa7d553fb68e33433e7
nyxb <dennis.ollhoff@joypix.gg>
2024-10-04 16:21:30 +0200

    ♻️ refactor(web): update import paths for content collections for better organization and maintainability
    🎨 style(web): improve gauge-circle component styles for better readability and maintainability
    
    🧹 chore(web): remove unused code and comments in rehype-component.ts for cleaner codebase
    
    🎨 style(web): update CSS styles for MDX components for improved consistency and readability

--- apps/web/lib/rehype-component.ts
+++ apps/web/lib/rehype-component.ts
@@ -1,22 +1,16 @@
-import fs from 'node:fs'
-import path from 'node:path'
-import type { UnistNode, UnistTree } from 'types/unist'
+import fs from 'fs'
+import path from 'path'
 import { u } from 'unist-builder'
 import { visit } from 'unist-util-visit'
 
-import { Index } from '../__registry__'
-import { styles } from '../registry/styles'
+import { Index } from '~/__registry__'
+import { styles } from '~/registry/styles'
+import type { UnistNode, UnistTree } from '~/types/unist'
 
 export function rehypeComponent() {
    return async (tree: UnistTree) => {
       visit(tree, (node: UnistNode) => {
-      // src prop overrides both name and fileName.
-         const { value: srcPath }
-        = (getNodeAttributeByName(node, 'src') as {
-           name: string
-           value?: string
-           type?: string
-        }) || {}
+         const { value: srcPath } = getNodeAttributeByName(node, 'src') || {}
 
          if (node.name === 'ComponentSource') {
             const name = getNodeAttributeByName(node, 'name')?.value as string
@@ -24,15 +18,16 @@ export function rehypeComponent() {
                | string
                | undefined
 
-            if (!name && !srcPath)
+            if (!name && !srcPath) {
                return null
+            }
 
             try {
                for (const style of styles) {
                   let src: string
 
                   if (srcPath) {
-                     src = srcPath
+                     src = srcPath as string
                   }
                   else {
                      const component = Index[style.name][name]
@@ -54,8 +49,8 @@ export function rehypeComponent() {
                   // TODO: Use @swc/core and a visitor to replace this.
                   // For now a simple regex should do.
                   source = source.replaceAll(
-              `~/registry/${style.name}/`,
-              '~/components/',
+              `@/registry/${style.name}/`,
+              '@/components/',
                   )
                   source = source.replaceAll('export default', 'export')
 
@@ -65,7 +60,6 @@ export function rehypeComponent() {
                         tagName: 'pre',
                         properties: {
                            __src__: src,
-                           __style__: style.name,
                         },
                         attributes: [
                            {
@@ -80,59 +74,8 @@ export function rehypeComponent() {
                               properties: {
                                  className: ['language-tsx'],
                               },
-                              children: [
-                                 {
-                                    type: 'text',
-                                    value: source,
-                                 },
-                              ],
-                           }),
-                        ],
-                     }),
-                  )
-               }
-            }
-            catch (error) {
-               console.error(error)
-            }
-         }
-
-         if (node.name === 'ComponentPreview') {
-            const name = getNodeAttributeByName(node, 'name')?.value as string
-
-            if (!name)
-               return null
-
-            try {
-               for (const style of styles) {
-                  const component = Index[style.name][name]
-                  const src = component.files[0]
-
-                  // Read the source file.
-                  const filePath = path.join(process.cwd(), src)
-                  let source = fs.readFileSync(filePath, 'utf8')
-
-                  // Replace imports.
-                  // TODO: Use @swc/core and a visitor to replace this.
-                  // For now a simple regex should do.
-                  source = source.replaceAll(
-              `~/registry/${style.name}/`,
-              '~/components/',
-                  )
-                  source = source.replaceAll('export default', 'export')
-
-                  // Add code as children so that rehype can take over at build time.
-                  node.children?.push(
-                     u('element', {
-                        tagName: 'pre',
-                        properties: {
-                           __src__: src,
-                        },
-                        children: [
-                           u('element', {
-                              tagName: 'code',
-                              properties: {
-                                 className: ['language-tsx'],
+                              data: {
+                                 meta: `event="copy_source_code"`,
                               },
                               children: [
                                  {
@@ -151,112 +94,63 @@ export function rehypeComponent() {
             }
          }
 
-         // if (node.name === "ComponentExample") {
-         //   const source = getComponentSourceFileContent(node)
-         //   if (!source) {
-         //     return
-         //   }
+         if (node.name === 'ComponentPreview' || node.name === 'BlockPreview') {
+            const name = getNodeAttributeByName(node, 'name')?.value as string
 
-         //   // Replace the Example component with a pre element.
-         //   node.children?.push(
-         //     u("element", {
-         //       tagName: "pre",
-         //       properties: {
-         //         __src__: src,
-         //       },
-         //       children: [
-         //         u("element", {
-         //           tagName: "code",
-         //           properties: {
-         //             className: ["language-tsx"],
-         //           },
-         //           children: [
-         //             {
-         //               type: "text",
-         //               value: source,
-         //             },
-         //           ],
-         //         }),
-         //       ],
-         //     })
-         //   )
+            if (!name) {
+               return null
+            }
 
-         //   const extractClassname = getNodeAttributeByName(
-         //     node,
-         //     "extractClassname"
-         //   )
-         //   if (
-         //     extractClassname &&
-         //     typeof extractClassname.value !== "undefined" &&
-         //     extractClassname.value !== "false"
-         //   ) {
-         //     // Extract className from string
-         //     // TODO: Use @swc/core and a visitor to extract this.
-         //     // For now, a simple regex should do.
-         //     const values = source.match(/className="(.*)"/)
-         //     const className = values ? values[1] : ""
+            try {
+               for (const style of styles) {
+                  const component = Index[style.name][name]
+                  const src = component.files[0]
 
-         //     // Add the className as a jsx prop so we can pass it to the copy button.
-         //     node.attributes?.push({
-         //       name: "extractedClassNames",
-         //       type: "mdxJsxAttribute",
-         //       value: className,
-         //     })
+                  // Read the source file.
+                  const filePath = path.join(process.cwd(), src)
+                  let source = fs.readFileSync(filePath, 'utf8')
 
-         //     // Add a pre element with the className only.
-         //     node.children?.push(
-         //       u("element", {
-         //         tagName: "pre",
-         //         properties: {},
-         //         children: [
-         //           u("element", {
-         //             tagName: "code",
-         //             properties: {
-         //               className: ["language-tsx"],
-         //             },
-         //             children: [
-         //               {
-         //                 type: "text",
-         //                 value: className,
-         //               },
-         //             ],
-         //           }),
-         //         ],
-         //       })
-         //     )
-         //   }
-         // }
+                  // Replace imports.
+                  // TODO: Use @swc/core and a visitor to replace this.
+                  // For now a simple regex should do.
+                  source = source.replaceAll(
+              `@/registry/${style.name}/`,
+              '@/components/',
+                  )
+                  source = source.replaceAll('export default', 'export')
 
-         // if (node.name === "ComponentSource") {
-         //   const source = getComponentSourceFileContent(node)
-         //   if (!source) {
-         //     return
-         //   }
-
-      //   // Replace the Source component with a pre element.
-      //   node.children?.push(
-      //     u("element", {
-      //       tagName: "pre",
-      //       properties: {
-      //         __src__: src,
-      //       },
-      //       children: [
-      //         u("element", {
-      //           tagName: "code",
-      //           properties: {
-      //             className: ["language-tsx"],
-      //           },
-      //           children: [
-      //             {
-      //               type: "text",
-      //               value: source,
-      //             },
-      //           ],
-      //         }),
-      //       ],
-      //     })
-      //   )
-      // }
+                  // Add code as children so that rehype can take over at build time.
+                  node.children?.push(
+                     u('element', {
+                        tagName: 'pre',
+                        properties: {
+                           __src__: src,
+                        },
+                        children: [
+                           u('element', {
+                              tagName: 'code',
+                              properties: {
+                                 className: ['language-tsx'],
+                              },
+                              data: {
+                                 meta: `event="copy_usage_code"`,
+                              },
+                              children: [
+                                 {
+                                    type: 'text',
+                                    value: source,
+                                 },
+                              ],
+                           }),
+                        ],
+                     }),
+                  )
+               }
+            }
+            catch (error) {
+               console.error(error)
+            }
+         }
       })
    }
 }
@@ -268,8 +162,9 @@ function getNodeAttributeByName(node: UnistNode, name: string) {
 function getComponentSourceFileContent(node: UnistNode) {
    const src = getNodeAttributeByName(node, 'src')?.value as string
 
-   if (!src)
+   if (!src) {
       return null
+   }
 
    // Read the source file.
    const filePath = path.join(process.cwd(), src)
