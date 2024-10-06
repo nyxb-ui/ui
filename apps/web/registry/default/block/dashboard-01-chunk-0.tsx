40004d92cf4cb7b8892b4ae3f7e1ddaaa200dd11
nyxb <dennis.ollhoff@joypix.gg>
2024-10-06 23:35:43 +0200

    💄lint: fix

--- apps/web/registry/default/block/dashboard-01-chunk-0.tsx
+++ apps/web/registry/default/block/dashboard-01-chunk-0.tsx
@@ -1,23 +1,22 @@
-'use client'
 
-import { DollarSign } from 'lucide-react'
-import { Card, CardContent, CardHeader, CardTitle } from '~/registry/default/ui/card'
+            'use client'
 
-export default function Component() {
-   return (
-      <Card x-chunk="dashboard-01-chunk-0">
-         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-               Total Revenue
-            </CardTitle>
-            <DollarSign className="text-muted-foreground size-4" />
-         </CardHeader>
-         <CardContent>
-            <div className="text-2xl font-bold">$45,231.89</div>
-            <p className="text-muted-foreground text-xs">
-               +20.1% from last month
-            </p>
-         </CardContent>
-      </Card>
-   )
-}
+            import {Card,CardHeader,CardTitle,CardContent} from "~/registry/default/ui/card"
+import {DollarSign} from "lucide-react"
+
+            export default function Component() {
+              return (<Card x-chunk="dashboard-01-chunk-0">
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                     <CardTitle className="text-sm font-medium">
+                        Total Revenue
+                     </CardTitle>
+                     <DollarSign className="text-muted-foreground size-4" />
+                  </CardHeader>
+                  <CardContent>
+                     <div className="text-2xl font-bold">$45,231.89</div>
+                     <p className="text-muted-foreground text-xs">
+                        +20.1% from last month
+                     </p>
+                  </CardContent>
+               </Card>)
+            }
--- apps/web/registry/miami/block/charts-01-chunk-1.tsx
+++ apps/web/registry/miami/block/charts-01-chunk-1.tsx
@@ -1,132 +1,130 @@
-'use client'
 
-import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '~/registry/miami//ui/card'
-import { ChartContainer, ChartTooltip, ChartTooltipContent } from '~/registry/miami//ui/chart'
+            'use client'
 
-export default function Component() {
-   return (
-      <Card
-         className="flex flex-col lg:max-w-md"
-         x-chunk="charts-01-chunk-1"
-      >
-         <CardHeader className="flex flex-row items-center gap-4 space-y-0 pb-2 [&>div]:flex-1">
-            <div>
-               <CardDescription>Resting HR</CardDescription>
-               <CardTitle className="flex items-baseline gap-1 text-4xl tabular-nums">
-                  62
-                  <span className="text-muted-foreground text-sm font-normal tracking-normal">
-                     bpm
-                  </span>
-               </CardTitle>
-            </div>
-            <div>
-               <CardDescription>Variability</CardDescription>
-               <CardTitle className="flex items-baseline gap-1 text-4xl tabular-nums">
-                  35
-                  <span className="text-muted-foreground text-sm font-normal tracking-normal">
-                     ms
-                  </span>
-               </CardTitle>
-            </div>
-         </CardHeader>
-         <CardContent className="flex flex-1 items-center">
-            <ChartContainer
-               config={{
-                  resting: {
-                     label: 'Resting',
-                     color: 'hsl(var(--chart-1))',
-                  },
-               }}
-               className="w-full"
+            import {Card,CardHeader,CardDescription,CardTitle,CardContent} from "~/registry/miami//ui/card"
+import {ChartContainer,ChartTooltip,ChartTooltipContent} from "~/registry/miami//ui/chart"
+import {LineChart,CartesianGrid,YAxis,XAxis,Line} from "recharts"
+
+            export default function Component() {
+              return (<Card
+               className="flex flex-col lg:max-w-md" x-chunk="charts-01-chunk-1"
             >
-               <LineChart
-                  accessibilityLayer
-                  margin={{
-                     left: 14,
-                     right: 14,
-                     top: 10,
-                  }}
-                  data={[
-                     {
-                        date: '2024-01-01',
-                        resting: 62,
-                     },
-                     {
-                        date: '2024-01-02',
-                        resting: 72,
-                     },
-                     {
-                        date: '2024-01-03',
-                        resting: 35,
-                     },
-                     {
-                        date: '2024-01-04',
-                        resting: 62,
-                     },
-                     {
-                        date: '2024-01-05',
-                        resting: 52,
-                     },
-                     {
-                        date: '2024-01-06',
-                        resting: 62,
-                     },
-                     {
-                        date: '2024-01-07',
-                        resting: 70,
-                     },
-                  ]}
-               >
-                  <CartesianGrid
-                     strokeDasharray="4 4"
-                     vertical={false}
-                     stroke="hsl(var(--muted-foreground))"
-                     strokeOpacity={0.5}
-                  />
-                  <YAxis hide domain={['dataMin - 10', 'dataMax + 10']} />
-                  <XAxis
-                     dataKey="date"
-                     tickLine={false}
-                     axisLine={false}
-                     tickMargin={8}
-                     tickFormatter={(value) => {
-                        return new Date(value).toLocaleDateString('en-US', {
-                           weekday: 'short',
-                        })
+               <CardHeader className="flex flex-row items-center gap-4 space-y-0 pb-2 [&>div]:flex-1">
+                  <div>
+                     <CardDescription>Resting HR</CardDescription>
+                     <CardTitle className="flex items-baseline gap-1 text-4xl tabular-nums">
+                        62
+                        <span className="text-muted-foreground text-sm font-normal tracking-normal">
+                           bpm
+                        </span>
+                     </CardTitle>
+                  </div>
+                  <div>
+                     <CardDescription>Variability</CardDescription>
+                     <CardTitle className="flex items-baseline gap-1 text-4xl tabular-nums">
+                        35
+                        <span className="text-muted-foreground text-sm font-normal tracking-normal">
+                           ms
+                        </span>
+                     </CardTitle>
+                  </div>
+               </CardHeader>
+               <CardContent className="flex flex-1 items-center">
+                  <ChartContainer
+                     config={{
+                        resting: {
+                           label: 'Resting',
+                           color: 'hsl(var(--chart-1))',
+                        },
                      }}
-                  />
-                  <Line
-                     dataKey="resting"
-                     type="natural"
-                     fill="var(--color-resting)"
-                     stroke="var(--color-resting)"
-                     strokeWidth={2}
-                     dot={false}
-                     activeDot={{
-                        fill: 'var(--color-resting)',
-                        stroke: 'var(--color-resting)',
-                        r: 4,
-                     }}
-                  />
-                  <ChartTooltip
-                     content={(
-                        <ChartTooltipContent
-                           indicator="line"
-                           labelFormatter={(value) => {
+                     className="w-full"
+                  >
+                     <LineChart
+                        accessibilityLayer
+                        margin={{
+                           left: 14,
+                           right: 14,
+                           top: 10,
+                        }}
+                        data={[
+                           {
+                              date: '2024-01-01',
+                              resting: 62,
+                           },
+                           {
+                              date: '2024-01-02',
+                              resting: 72,
+                           },
+                           {
+                              date: '2024-01-03',
+                              resting: 35,
+                           },
+                           {
+                              date: '2024-01-04',
+                              resting: 62,
+                           },
+                           {
+                              date: '2024-01-05',
+                              resting: 52,
+                           },
+                           {
+                              date: '2024-01-06',
+                              resting: 62,
+                           },
+                           {
+                              date: '2024-01-07',
+                              resting: 70,
+                           },
+                        ]}
+                     >
+                        <CartesianGrid
+                           strokeDasharray="4 4"
+                           vertical={false}
+                           stroke="hsl(var(--muted-foreground))"
+                           strokeOpacity={0.5}
+                        />
+                        <YAxis hide domain={['dataMin - 10', 'dataMax + 10']} />
+                        <XAxis
+                           dataKey="date"
+                           tickLine={false}
+                           axisLine={false}
+                           tickMargin={8}
+                           tickFormatter={(value) => {
                               return new Date(value).toLocaleDateString('en-US', {
-                                 day: 'numeric',
-                                 month: 'long',
-                                 year: 'numeric',
+                                 weekday: 'short',
                               })
                            }}
                         />
-                     )}
-                     cursor={false}
-                  />
-               </LineChart>
-            </ChartContainer>
-         </CardContent>
-      </Card>
-   )
-}
+                        <Line
+                           dataKey="resting"
+                           type="natural"
+                           fill="var(--color-resting)"
+                           stroke="var(--color-resting)"
+                           strokeWidth={2}
+                           dot={false}
+                           activeDot={{
+                              fill: 'var(--color-resting)',
+                              stroke: 'var(--color-resting)',
+                              r: 4,
+                           }}
+                        />
+                        <ChartTooltip
+                           content={(
+                              <ChartTooltipContent
+                                 indicator="line"
+                                 labelFormatter={(value) => {
+                                    return new Date(value).toLocaleDateString('en-US', {
+                                       day: 'numeric',
+                                       month: 'long',
+                                       year: 'numeric',
+                                    })
+                                 }}
+                              />
+                           )}
+                           cursor={false}
+                        />
+                     </LineChart>
+                  </ChartContainer>
+               </CardContent>
+            </Card>)
+            }
